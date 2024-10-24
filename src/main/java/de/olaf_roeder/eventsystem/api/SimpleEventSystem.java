3b515181299075f647d50e9bee222432d333b9f4
Olaf Roeder <github@olaf-roeder.de>
2024-10-24 23:00:27 +0200

    added some logging on debug-level;

--- src/main/java/de/olaf_roeder/eventsystem/api/SimpleEventSystem.java
+++ src/main/java/de/olaf_roeder/eventsystem/api/SimpleEventSystem.java
@@ -3,9 +3,11 @@ package de.olaf_roeder.eventsystem.api;
 import de.olaf_roeder.concurrency.EventExecutorService;
 import de.olaf_roeder.eventsystem.engine.EventPublisher;
 import de.olaf_roeder.eventsystem.engine.EventRepository;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.function.Consumer;
 
+@Slf4j
 public class SimpleEventSystem {
 
     private final EventRepository eventRepository;
@@ -26,13 +28,16 @@ public class SimpleEventSystem {
 
     public final <T> void addEventRunnable(Class<T> eventClass, Runnable runnable) {
         eventRepository.addEventRunnable(eventClass, runnable);
+        log.debug("Successfully added {} as EventRunnable", eventClass);
     }
 
     public final <T> void addEventConsumer(Class<T> eventClass, Consumer<T> eventConsumer) {
         eventRepository.addEventConsumer(eventClass, eventConsumer);
+        log.debug("Successfully added {} as EventConsumer", eventClass);
     }
 
     public final <T> void publishEvent(T event) {
         eventPublisher.publish(event);
+        log.debug("Successfully published event {}", event);
     }
 }
